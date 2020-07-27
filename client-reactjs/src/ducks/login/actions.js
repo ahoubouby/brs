504b5d6aa6083c865383a3cf28c46e6831e7937d
ahoubouby <ahoubouby@gmail.com>
2020-07-27 16:54:40 +0100

    from switche statement to strategies pattern

--- client-reactjs/src/ducks/login/actions.js
+++ client-reactjs/src/ducks/login/actions.js
@@ -1,3 +1,3 @@
 export const LOGGED_IN_SUCCESS = "@account/LOGGED_IN_SUCCESS";
 export const LOGGIN_IN_FAILED = "@account/LOGGED_IN_FAILD";
-export const LOGGED_OUT_SUCCESS = "@account/LOGGED_IN_SUCCESS";
+export const LOGGED_OUT_SUCCESS = "@account/LOGGED_OUT_SUCCESS";
--- client-reactjs/src/ducks/login/reducer.js
+++ client-reactjs/src/ducks/login/reducer.js
@@ -1,25 +1,34 @@
-import * as actions from "./actions";
-
 const initialState = {
   isLoggedIn: false,
   users: [],
+  errors: {},
 };
 
+const transformInCaseOfSuccess = (state, action) => ({
+  isLoggedIn: true,
+  users: [...action.payload],
+});
+
+const transformInCaseOfFailed = (state, action) => ({
+  status: "false",
+  users: [],
+});
+
+const transformInCaseOfLoggout = (state, action) => ({
+  ...state,
+  errors: action.payload.errors, // capture errors here
+});
+
+// state transformation (state, action) => state
+const strategies = {
+  LOGGED_IN_SUCCESS: transformInCaseOfSuccess,
+  LOGGIN_IN_FAILED: transformInCaseOfFailed,
+  LOGGED_OUT_SUCCESS: transformInCaseOfLoggout,
+  // add more transform functions
+  __default__: (state, _) => state,
+};
+// refactor reduce, get ride of switch statement by replacement of stratgie pattern
 export default function reducer(state = initialState, action) {
-  switch (action.type) {
-    case actions.LOGGED_IN_SUCCESS:
-      return {
-        isLoggedIn: true,
-        users: [...action.payload],
-      };
-
-    case actions.LOGGED_OUT_SUCCESS:
-      return {
-        status: "false",
-        users: [],
-      };
-
-    default:
-      return state;
-  }
+  const transform = strategies[action.type] || strategies.__default__;
+  return transform(state, action);
 }
