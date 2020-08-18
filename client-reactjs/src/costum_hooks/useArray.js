3d678ddfabfea43f9b7644c62d9ccc318b180338
ahoubouby <ahoubouby@gmail.com>
2020-08-18 09:24:21 +0100

    Custom hooks: add useArray

---
+++ client-reactjs/src/costum_hooks/useArray.js
@@ -0,0 +1,24 @@
+export { useCallback, useState } from "react";
+
+const useArray = (initValue) => {
+  const [value, setValue] = useState(initValue);
+  const add = useCallback(
+    (newValue) => setValue((prev) => [...prev, newValue]),
+    []
+  );
+
+  const clear = useCallback(() => setValue(() => []), []);
+
+  const removeById = useCallback((id) =>
+    setValue((prev) => prev.filter((el) => el && el.id !== id), [])
+  );
+
+  const removeIndex = useCallback(
+    (index) => setValue((arr) => arr.filter((v, i) => i !== index)),
+    []
+  );
+
+  return [value, add, clear, removeById, removeIndex];
+};
+
+export default useArray;
